c00f35edb874815a88c97ca578e6b2b91bc12c81
Matt Landreman <mattland@wireless-10-105-115-109.umd.edu>
2017-09-14 13:52:21 -0400

    Initial commit

---
+++ makefile
@@ -0,0 +1,54 @@
+ifdef NERSC_HOST
+        HOSTNAME = $(NERSC_HOST)
+else ifeq ($(CLUSTER),DRACO)
+	HOSTNAME="draco"
+else
+        HOSTNAME="laptop"
+endif
+
+
+ifeq ($(HOSTNAME),cori)
+        FC = ftn
+	LIBSTELL_DIR = /global/homes/l/landrema/stellopt/LIBSTELL/Release
+        PARVMEC_DIR = /global/homes/l/landrema/stellopt/PARVMEC/Release
+        EXTRA_COMPILE_FLAGS =
+        EXTRA_LINK_FLAGS =
+else ifeq ($(HOSTNAME),draco)
+        FC =  mpiifort
+	LIBSTELL_DIR = /u/mlan/stellopt/LIBSTELL/Release
+        PARVMEC_DIR = /u/mlan/stellopt/PARVMEC/Release
+        EXTRA_COMPILE_FLAGS = -I${NETCDF_HOME}/include
+        EXTRA_LINK_FLAGS = -L${NETCDF_HOME}/lib -lnetcdf -lnetcdff -L${HDF5_HOME}/lib -lhdf5_fortran -lhdf5 -lhdf5_hl -lhdf5hl_fortran
+else
+	FC = mpif90
+	LIBSTELL_DIR = /Users/mattland/stellopt/LIBSTELL/Release
+	PARVMEC_DIR = /Users/mattland/stellopt/PARVMEC/Release
+	EXTRA_COMPILE_FLAGS = -I/opt/local/include -ffree-line-length-none -g -O0
+	EXTRA_LINK_FLAGS =  -L/opt/local/lib -lnetcdff  -lnetcdf -framework Accelerate -lscalapack -g -O0
+endif
+
+# End of system-dependent variable assignments
+
+.PHONY: all clean
+
+all: parvmec_minimal_example
+
+%.o: %.f90
+	$(FC) $(EXTRA_COMPILE_FLAGS) -I $(LIBSTELL_DIR) -I $(PARVMEC_DIR) -c $<
+
+libparvmec.a:
+	ar -cru libparvmec.a $(PARVMEC_DIR)/*.o
+
+parvmec_minimal_example: parvmec_minimal_example.o libparvmec.a stellopt_reinit_vmec.o stellopt_bcast_vmec.o
+	$(FC) -o parvmec_minimal_example parvmec_minimal_example.o stellopt_reinit_vmec.o stellopt_bcast_vmec.o $(LIBSTELL_DIR)/libstell.a libparvmec.a $(EXTRA_LINK_FLAGS)
+
+clean:
+	rm -f *.o *.mod *.MOD *~ parvmec_minimal_example libparvmec.a
+
+test_make:
+	@echo HOSTNAME is $(HOSTNAME)
+	@echo FC is $(FC)
+	@echo EXTRA_COMPILE_FLAGS is $(EXTRA_COMPILE_FLAGS)
+	@echo EXTRA_LINK_FLAGS is $(EXTRA_LINK_FLAGS)
+	@echo LIBSTELL_DIR is $(LIBSTELL_DIR)
+	@echo PARVMEC_DIR is $(PARVMEC_DIR)
---
+++ parvmec_minimal_example.f90
@@ -0,0 +1,54 @@
+program parvmec_minimal_example
+
+  use parallel_vmec_module, only: InitRunVmec, FinalizeRunVmec, RUNVMEC_COMM_WORLD
+  use vmec_params, only: restart_flag, readin_flag, timestep_flag, output_flag, cleanup_flag, reset_jacdt_flag
+  use safe_open_mod
+
+  implicit none
+
+  include 'mpif.h'
+
+  integer :: ierr, rank, num_procs, ictrl(5), myseq=0, iteration, iunit=12
+  logical :: master, lfreeb=.false.
+  character(len=*), parameter :: file_str = 'lpku_A6_lowiota_5.min_newP'
+  character(len = 128)    :: reset_str
+
+  call MPI_INIT(ierr)
+  call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierr)
+  call MPI_COMM_SIZE(MPI_COMM_WORLD, num_procs, ierr)
+  CALL MPI_ERRHANDLER_SET(MPI_COMM_WORLD,MPI_ERRORS_RETURN,ierr) ! This line is used in stellopt_main(). If not included, the first call to runvmec dies at the end of fileout_par().
+  master = (rank==0)
+  if (master) print *,"Detected",num_procs,"procs."
+
+  ! Set ictrl array as done in stellopt_init.f90, to just read in the input file.
+  ictrl(1) = restart_flag + readin_flag + reset_jacdt_flag
+  ictrl(2) = 0; ictrl(3) = 50; ictrl(4) = 0; ictrl(5) = myseq
+!  IF (master) THEN
+!     CALL safe_open(iunit,ierr,'threed1.'//TRIM(file_str),'unknown','formatted')
+!     CLOSE(iunit)
+!  END IF
+  call MPI_BARRIER(MPI_COMM_WORLD, ierr)
+  call runvmec(ictrl,file_str,.false.,MPI_COMM_SELF,'')
+
+  if (master) print *,"Done reading input file."
+
+  do iteration = 1,5
+     if (master) print *,"Beginning call",iteration," to parvmec."
+     call stellopt_bcast_vmec(0,MPI_COMM_WORLD,ierr)
+     call stellopt_reinit_vmec()
+
+     ! Now set ICTRL Array as done in stellopt_paraexe.f90
+     ictrl(1) = restart_flag + timestep_flag + output_flag + reset_jacdt_flag
+     ictrl(2) = 0; ictrl(3) = -1; ictrl(4) = -1; ictrl(5) = myseq
+     reset_str = ''
+     call MPI_BARRIER(MPI_COMM_WORLD, ierr)
+     call InitRunVmec(MPI_COMM_WORLD,lfreeb)
+     call runvmec(ictrl,file_str,.true.,RUNVMEC_COMM_WORLD,reset_str)
+     call FinalizeRunVmec(RUNVMEC_COMM_WORLD)
+  end do
+
+  call MPI_FINALIZE(ierr)
+
+  if (master) print *,"Done."
+
+end program parvmec_minimal_example
